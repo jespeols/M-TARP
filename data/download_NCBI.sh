0f1204a0303fb80917e3d54939e8621faee87f32
jespeols <jesperolsson9@hotmail.com>
2024-09-03 15:47:30 +0200

    update data scripts

--- data/download_NCBI.sh
+++ data/download_NCBI.sh
@@ -1,5 +1,9 @@
 echo "Downloading data from NCBI"
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 mkdir -p raw
 # Note: Link may need to be updated, check NCBI for the latest link
 wget -O raw/NCBI.tsv \
-https://ftp.ncbi.nlm.nih.gov/pathogen/Results/Escherichia_coli_Shigella/PDG000000004.4460/Metadata/PDG000000004.4460.metadata.tsv
+https://ftp.ncbi.nlm.nih.gov/pathogen/Results/Escherichia_coli_Shigella/PDG000000004.4469/Metadata/PDG000000004.4469.metadata.tsv
--- data/unzip_TESSy.sh
+++ data/unzip_TESSy.sh
@@ -1,6 +1,8 @@
 echo "Extracting TESSy data"
-# assumes zip is in same folder as this script
-mkdir -p raw
-unzip AMR_TEST.zip && mv AMR_TEST.csv raw/AMR_TEST.csv
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"/raw
+
+unzip AMR_TEST.zip
 rm AMR_TEST.zip
 rm AMR_TEST.zip:Zone.Identifier
